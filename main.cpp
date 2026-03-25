a50308bb493bf551db90c16c200196f8672f5fa6
Bannatte <32626837+Bannatte@users.noreply.github.com>
2026-03-24 23:45:12 -0700

    Refactor main.cpp to find largest and smallest numbers
    Refactor code to find and print the largest and smallest of three numbers.

--- main.cpp
+++ main.cpp
@@ -1,11 +1,39 @@
-int num1, num2, num3;
-cin >> num1 >> num2 >> num3;
-int min;
+#include <iostream>
+using namespace std;
 
-if ((num1 < num2) && (num1 < num3)) {
-  min = num1;
-} else {
-  min = (num2 < num3) ? num2 : num3;
+int main() {
+  int num1, num2, num3;
+  cin >> num1 >> num2 >> num3;
+  int first, second, third;
+
+  if (num1 >= num2 && num1 >= num3) {
+    first = num1;
+    if (num2 >= num3) {
+      second = num2;
+      third = num3;
+    } else {
+      second = num3;
+      third = num2;
+    }
+  } else if (num2 >= num1 && num2 >= num3) {
+      first = num2;
+      if (num1 >= num3) {
+          second = num1;
+          third = num3;
+      } else {
+          second = num3;
+          third = num1;
+      }
+  } else { // num3 is the largest
+      first = num3;
+      if (num1 >= num2) {
+          second = num1;
+          third = num2;
+      } else {
+          second = num2;
+          third = num1;
+      }
+  }
+  cout << first << " " << second << " " << third << endl;
+  return 0;
 }
-
-return min
